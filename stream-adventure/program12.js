d98f65f596cbc694ace6bfb56626ca28b4613fbc
rcanepa <rcanepag@gmail.com>
2014-11-04 23:27:17 -0300

    Duplex and Through

---
+++ stream-adventure/program12.js
@@ -0,0 +1,24 @@
+var through = require('through');
+var duplexer = require('duplexer');
+
+
+module.exports = function(counter){
+	var countries = {};
+	var tr = through(write, end);
+	
+	function write (buffer) {
+		var country = buffer.country;
+		var keys = Object.keys(countries);
+		if (country in countries)
+			countries[country] += 1;
+		else
+			countries[country] = 1;
+		// or countries[country] = (countries[country] || 0) + 1;
+	}
+
+	function end (){
+		counter.setCounts(countries);
+	}
+
+	return duplexer(tr, counter);
+};
